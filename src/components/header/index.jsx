13040e38c4e49dcbbe5bd3ba9029aa286e406864
MrMaximora <maxenceouvrarddu16@outlook.fr>
2023-04-24 17:50:04 +0200

    headerV1

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,8 +3,21 @@ import "./header.scss"
 
 export default function Header() {
     return (
-        <div className="header">
-           
+        <div className='header'>
+           <div className='header__logo'>
+             <img src='../public/radreams.png' alt="Logo RAdreams"/>
+           </div>
+            <div className="structure">
+                <div className="header__phone">
+                        <p>06 34 10 83 49 - 06 72 89 61 65</p>
+                    </div>          
+                <nav className='header__nav'>
+                    <Link className="header__nav__link" to= '/'>Accueil</Link>
+                    <Link className="header__nav__link" to= '/portrait'>portrait</Link>
+                    <Link className="header__nav__link" to='/applications-et-outils'>Applications et Outils</Link>
+                    <Link className="header__nav__link" to='/contact'>Contact</Link>
+                </nav>
+            </div>
         </div>
     )
 }
