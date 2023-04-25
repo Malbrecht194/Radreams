340c66255ed9ee05076a024e2e7b98b06fc08591
Maxime ALBRECHT <albrechtmaxime373@gmail.com>
2023-04-25 11:42:31 +0200

    Amélioration page daccueil / création de banner / correction du button / ajout couleur / changement de la font prestage

--- src/pages/Accueil/index.jsx
+++ src/pages/Accueil/index.jsx
@@ -2,20 +2,23 @@ import { useLoaderData } from "react-router-dom"
 import Button from "../../components/Button/main"
 import "./accueil.scss"
 import Tools from "../../components/tools"
+import Banner from "../../components/banner"
 
 export default function Accueil() {
     const tools = useLoaderData() // loader pour charger les textes des outils de l'acccueil
     return (
         <>
+            
             <section className="toolCards">
-
-                {tools.map((tool, index) => (
-                    <Tools key={index} tool={tool} />
-                ))}
-
-
+                <Banner bannerTitle="Nos principaux outils" />
+                <div className="toolCards__container">
+                    {tools.map((tool, index) => (
+                        <Tools key={index} tool={tool} />
+                    ))}
+                </div>
+                <Button className='toolsCards__button' buttonTitle="Prendre RDV" buttonLink='/contact' />
             </section>
-            <Button buttonTitle="Prendre RDV" buttonLink='/contact' />
+
         </>
     )
 }
