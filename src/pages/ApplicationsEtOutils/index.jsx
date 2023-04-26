8ca3e701353d686da482a1a468875eb78ade5d32
Maxime ALBRECHT <albrechtmaxime373@gmail.com>
2023-04-26 16:48:06 +0200

    fix avant merge

--- src/pages/ApplicationsEtOutils/index.jsx
+++ src/pages/ApplicationsEtOutils/index.jsx
@@ -1,9 +1,19 @@
+import { useLoaderData } from "react-router-dom"
 import "./applicationsEtOutils.scss"
+import Questions from "../../components/questions"
 
 export default function ApplicationsEtOutils() {
+    const questionaryData = useLoaderData()
     return (
         <>
-            
+            {questionaryData.map((data, index3) => (
+                <section key={index3}>
+                    <Questions data={data} />
+                </section>
+            ))}
         </>
     )
+}
+export async function loader1() {
+    return (await import("../../data/questionary.json")).default // chargement au moment ou on l'appel
 }
